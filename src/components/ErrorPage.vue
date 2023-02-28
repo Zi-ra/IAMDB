05e22d819c69b318110b938f884ed9c95f9e3ed2
Zi-ra <zi.9876.ra@gmail.com>
2023-03-01 02:55:02 +0330

    fifth commit

--- src/components/ErrorPage.vue
+++ src/components/ErrorPage.vue
@@ -9,3 +9,33 @@
     </div>
 </template>
 
+<style scoped>
+    div{
+        justify-items:center;
+        align-items: center;
+    }
+    h1{
+        font-size: 128px;
+        font-weight: 700;
+        line-height: 150px;
+        opacity: 40%;
+        color: var(--primary-text-color);
+    }
+    h2{
+        font-size: 48px;
+        font-weight: 700;
+        line-height: 56.25px;
+        color: var(--accent-color);
+    }
+
+    @media only screen and (max-width: 767px){
+        h1{
+            font-size: 96px;
+            line-height: 112.5px;
+        }
+        h2{
+            font-size: 40px;
+            line-height: 46.88px;
+        }
+    }
+</style>
