2aad7edb2ca5a992de68d3ff8451a1cec270f71d
Zi-ra <zi.9876.ra@gmail.com>
2023-03-01 02:53:00 +0330

    fourth commit

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,11 @@
+<script>
+    import '../assets/style.css'
+</script>
+
+<template>
+    <div class="d-flex col center w-100">
+        <h1>404</h1>
+        <h2>Movie not found</h2>
+    </div>
+</template>
+
