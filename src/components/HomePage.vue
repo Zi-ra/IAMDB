a6c039139ee267fbe9b2fe3bca65d1d0cda5eabc
Zi-ra <zi.9876.ra@gmail.com>
2023-03-01 02:48:44 +0330

    third commit

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -17,5 +17,43 @@
 </template>
 
 <style scoped>
-
+    div{
+        padding: 0 120px;
+        justify-content: center;
+        align-items: center;
+    }
+    h1{
+        margin-bottom: 120px;
+        align-self: flex-end;
+        font-size: 96px;
+        font-weight: 700;
+        line-height: 112.5px;
+    }
+    .input-holder{
+        padding: 0;
+    }
+    input{
+        flex: 2 0 83%;
+        padding: 18px 32px;
+        border-radius: 100px;
+        border: 1px solid #EEEBDD;
+        background-color: transparent;
+        color: var(--primary-text-color);
+        outline: none;
+    }
+    button{
+        flex: 0 2 17%;
+        justify-content: space-between;
+        padding: 18px 32px;
+        margin-left: 12px;
+        border-radius: 100px;
+        font-size: 21px;
+        font-weight: 400;
+        line-height: 24.61px;
+        background-color: var(--accent-color);
+        transition: 0.3s;
+    }
+    button:hover{
+        background-color: #8E0D0D;
+    }
 </style>
