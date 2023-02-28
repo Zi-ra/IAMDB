a4c9516dfafe0da96b52553308730ac1189949da
Zi-ra <zi.9876.ra@gmail.com>
2023-03-01 02:46:27 +0330

    second commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import '../assets/style.css';
+    import SearchIcon from './icons/SearchIcon.vue';
+</script>
+
+<template>
+    <div class="d-flex col center w-100">
+        <h1>IAMDB</h1>
+        <div class="input-holder d-flex w-100">
+            <input type="text" placeholder="Type the name of your favorite movie ...">
+            <button class="d-flex text-capitalize">
+                search
+                <SearchIcon/>
+            </button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
