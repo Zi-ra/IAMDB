6e636da2bbdb39f97027aa6ed84728890ef9483a
Zi-ra <zi.9876.ra@gmail.com>
2023-03-01 09:41:14 +0330

    eighth commit

---
+++ src/components/MoviePage.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import '../assets/style.css';
+    import HearthIcon from './icons/HearthIcon.vue';
+    import PlayIcon from './icons/PlayIcon.vue';
+    import ShareIcon from './icons/ShareIcon.vue';
+    import StarIcon from './icons/StarIcon.vue';
+    import ToRightIcon from './icons/ToRightIcon.vue';
+    import Vector from './icons/Vector.vue';
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>
