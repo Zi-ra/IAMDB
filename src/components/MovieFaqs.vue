5c7e5b4749784184cd795787f91d78a9bd2b319b
Zi-ra <zi.9876.ra@gmail.com>
2023-03-01 11:07:14 +0330

    eleventh commit

---
+++ src/components/MovieFaqs.vue
@@ -0,0 +1,59 @@
+<script setup>
+    import '../assets/style.css';
+</script>
+
+<template>
+    <div class="container">
+        <h2 class="movie-faqs-title title m-title">FAQs</h2>
+        <ul class="movie-faqs-list list-unstyled">
+            <li class="faqs-list-items">
+                <details class="faqs-holder">
+                    <summary class="faqs-qn list-unstyled"></summary>
+                    <p class="faqs-answ"></p>
+                </details>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+    .movie-faqs-title{
+    margin-top: 50px;
+    }
+    .faqs-holder{
+        width: fit-content;
+    }
+    .movie-faqs-list{
+        padding: 0px;
+        margin: 0px;
+        
+    }
+    .faqs-qn{
+        font-size: 21px;
+        font-weight: 700;
+        line-height: 25px;
+        padding: 12px 0;
+        cursor: pointer;
+    }
+    .faqs-answ{
+        padding: 6px 0 6px 28px;
+        margin: 0;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 19px;
+        color: rgba(238, 235, 221, 0.6);
+        border-left: 4px solid var(--accent-color);
+    }
+    @media screen {
+        .faqs-qn{
+            font-size: 18px;
+            line-height: 21px;
+            padding-right: 25px;
+        }
+        .faqs-answ{
+            padding: 6px 8px 6px 24px;
+            font-size: 12px;
+            line-height: 14px;
+        }
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../components/HomePage.vue'
-import ErrorPage from '../components/ErrorPage.vue'
+import HomePage from '../components/HomePage.vue';
+import ErrorPage from '../components/ErrorPage.vue';
+import MoviePage from '../components/MoviePage.vue';
 
 const routes = [
     {
@@ -13,6 +14,11 @@ const routes = [
         name: 'Error',
         component: ErrorPage
     },
+    {
+        path: '/movie/:id',
+        name: 'Movie Detail',
+        component : MoviePage
+    }
 ]
 
 const router = createRouter({
