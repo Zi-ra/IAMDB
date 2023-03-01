4a592eb05633dd850a3113d2cb04de92614053ad
Zi-ra <zi.9876.ra@gmail.com>
2023-03-01 10:36:50 +0330

    ninth commit

--- src/components/MoviePage.vue
+++ src/components/MoviePage.vue
@@ -1,17 +1,29 @@
 <script setup>
     import '../assets/style.css';
-    import HearthIcon from './icons/HearthIcon.vue';
-    import PlayIcon from './icons/PlayIcon.vue';
-    import ShareIcon from './icons/ShareIcon.vue';
-    import StarIcon from './icons/StarIcon.vue';
-    import ToRightIcon from './icons/ToRightIcon.vue';
-    import Vector from './icons/Vector.vue';
-
 </script>
 
 <template>
-
+    <div>
+        <div class="movie-bg-img-holder"></div>
+        <MovieInfo/>
+        <MoviePhotos/>
+        <movieSimilar/>
+        <MovieFaqs/>
+    </div>
 </template>
 
 <style scoped>
+    .movie-bg-img-holder{
+        background-image: linear-gradient(180deg, rgba(27, 23, 23, 0)58.33%, rgba(27, 23, 23, 0.8)71.23%, var(--bg-color) 100% );
+        background-size: cover;
+        background-position: top;
+        background-repeat: no-repeat;
+        height: calc(var(--d-bg-height)*2);
+    }
+
+    @media only screen and (max-width: 767px) {
+        .movie-bg-img-holder{
+            height: calc(var(--m-bg-height)*0.77);
+        }
+    }
 </style>
